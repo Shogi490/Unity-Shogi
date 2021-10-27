9ffab60eea49b4281ca27fc9fa4b0112f5a7e5cf
Nicholas Sarubbi <85262920+nsarubbi@users.noreply.github.com>
2021-10-26 21:03:39 -0700

    board with pieces at bottom
    new

---
+++ Assets/Piece Scriptable Objects/ShogiPiece.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "ShogiPiece")]
+public class ShogiPiece : ScriptableObject
+{
+    public Sprite Sprite;
+    public string Name;
+    public int Value;
+    public MovementOption[] movementOptions;
+    public ShogiPiece PromotedPiece;
+}
+
+public enum MovementOption
+{
+    BKing,
+    BGoldGeneral,
+    BSilverGeneral,
+    BKnight,
+    BLance,
+    BBishop,
+    BRook,
+    BPawn,
+    WKing,
+    WGoldGeneral,
+    WSilverGeneral,
+    WKnight,
+    WLance,
+    WBishop,
+    WRook,
+    WPawn,
+    None
+}
